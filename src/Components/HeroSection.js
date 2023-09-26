b6c2f49210c58800eca0fe79af6b435cea3f36ba
Ehtisham <ehtishamsattar888@gmail.com>
2023-09-26 18:46:14 +0500

    heroSectionUpdate2

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -4,7 +4,7 @@ const HeroSection = () => {
     return (
         <>
 
-            <h1 className="m-10 mt-32 mb-1 text-center text-2xl font-extrabold tracking-tight leading-none text-black md:text-2xl lg:text-5xl dark:text-black" style={{ "fontFamily": "Inter Semibold" }}>We invest in our People’s dreams</h1>
+            <h1 className="m-10 mt-32 mb-1 text-center text-2xl font-extrabold tracking-tight leading-none text-black md:text-2xl lg:text-5xl dark:text-black bg-white" style={{ "fontFamily": "Inter Semibold" }}>We invest in our People’s dreams</h1>
             <section className="bg-white dark:bg-white " style={{ "fontFamily": "Inter Extra" }}>
                 <div className="py-8 px-4 mx-auto max-w-screen text-center lg:py-16 lg:px-12" style={{"backgroundColor": "rgb(248, 248, 248)"}}>
                     <a href="/signUp" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-white rounded-full dark:bg-black dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
@@ -14,7 +14,7 @@ const HeroSection = () => {
                     {/* <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1> */}
                     <p className="mb-8 text-lg text-black lg:text-xl sm:px-16 xl:px-48 dark:text-black font-bold">Discover the world through the words of explorers. Welcome to the Travel Log App blog!</p>
                     <div className="flex flex-col mb-4 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                        <a href="/login" className="bg-black text-white inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                        <a href="/signUp" className="bg-black text-white inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             Learn more
                             <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         </a>
